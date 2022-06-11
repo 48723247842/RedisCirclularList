058a1c577165d1aaf1101eb632249fa0c8dfef2a
48723247842 <48723247842@protonmail.com>
2022-06-11 17:21:15 -0400

    3

--- pypiUpload.sh
+++ pypiUpload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-rm -rf build
-rm -rf dist
+rm -rf build || true
+rm -rf dist || true
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+# https://twine.readthedocs.io/en/stable/
+
+token=$(cat /Users/morpheous/Tresors/Misc/Personal/7435171-PyPiToken.txt)
+python3 -m twine upload \
+-u "__token__" \
+-p "$token" \
+--repository-url https://upload.pypi.org/legacy/ dist/*
